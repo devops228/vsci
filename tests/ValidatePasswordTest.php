aee52737fa1809d32cc620864206321c91537386
devops228 <devops228@gmail.com>
2018-11-12 23:52:41 +1100

    add failing test

--- tests/ValidatePasswordTest.php
+++ tests/ValidatePasswordTest.php
@@ -5,9 +5,7 @@ class ValidatePasswordTest extends \PHPUnit\Framework\TestCase
 {
     public function testValidLength(){
        $validPass = new ValidatePassword();
-       $this->assertFalse($validPass->validLength('1234')); 
+       $this->assertFalse($validPass->validLength('123456789')); 
     }
 }
-
-
 ?>
