e4e3ce938e61daac01814172e2b6d289e4067872
devops228 <rizi@Rizwans-MBP.gateway>
2018-11-12 23:14:59 +1100

    inital comments

---
+++ tests/ValidatePasswordTest.php
@@ -0,0 +1,12 @@
+<?php
+require './src/ValidatePassword.php';
+
+class ValidatePasswordTest extends \PHPUnit\Framework\TestCase
+{
+    public function testValidLength(){
+       $validPass = new ValidatePassword();
+       $this->assertFalse($validPass->validLength('1234')); 
+    }
+}
+
+?>
