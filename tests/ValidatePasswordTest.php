e88defb6cd909765862a18ac430fb111cd427f3e
devops228 <devops228@gmail.com>
2018-11-13 23:15:21 +1100

    fix the test class

--- tests/ValidatePasswordTest.php
+++ tests/ValidatePasswordTest.php
@@ -5,7 +5,7 @@ class ValidatePasswordTest extends \PHPUnit\Framework\TestCase
 {
     public function testValidLength(){
        $validPass = new ValidatePassword();
-       $this->assertFalse($validPass->validLength('123456789')); 
+       $this->assertFalse($validPass->validLength('1234')); 
     }
 }
 ?>
